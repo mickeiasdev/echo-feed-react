40ba0a0a31299b7afd8dea438c29a40e332ebd83
Mickeias Dev <mickeiasdev@gmail.com>
2025-07-26 22:12:10 -0300

    Refactor Comment and Post components to handle dynamic comments and pass content as props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,32 +1,58 @@
+import { useState } from "react";
+
 // biblioteca de formatação de datas
 import { format, formatDistanceToNow } from "date-fns";
 import ptBr from "date-fns/locale/pt-BR";
 
-import styles from "./Post.module.css"
 import { Comment } from "./Comment";
 import { Avatar } from "./Avatar";
+
+import styles from "./Post.module.css"
 
 // author: { avatarUrl: ""; name: ""; role: ""; }
 // publishedAt: Date;
 // content: "";
 
 export function Post({ author, content, publishedAt }) {
+    const [comments, setComments] = useState([]);
+
+    const [newComment, setNewComment] = useState("");
+
+    const handleNewComment = (event) => {
+        event.preventDefault();
+
+        setNewComment(event.target.value);
+    }
+
+    const handleSubmitForm = (event) => {
+        // previne o comportamento padrão do formulário
+        event.preventDefault();
+
+        // aqui você poderia pegar o valor do textarea e adicionar como um novo comentário
+        setComments([...comments, newComment]);
+        setNewComment("");
+    }
+
     //formata a data de publicação
     //exemplo: 26 de julho às 20:38h
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", { locale: ptBr });
+
     // calcula o tempo desde a publicação
     //exemplo: há 1 dia, há 3 horas, há 2 meses
     const publishedTime = formatDistanceToNow(publishedAt, { locale: ptBr, addSuffix: true })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <Avatar src={author.avatarUrl} />
+
                     <div className={styles.authorInfo}>
                         <strong>{author.name}</strong>
                         <span>{author.role}</span>
                     </div>
                 </div>
+
                 {/* toISOString() converte a data para o formato ISO 8601 */}
                 <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
                     {publishedTime} atrás
@@ -44,6 +70,8 @@ export function Post({ author, content, publishedAt }) {
                             </p>
                         )
                     } else if (line.type === "hashtag") {
+                        // quando indentificamos que o tipo é "hashtag", renderizamos um conjunto de links em uma div
+                        // com um estilo de flexbox para exibir as hashtags lado a lado
                         return (
                             <div style={{ display: "flex", gap: ".5rem" }} key={index}>
                                 {line.content.map((hashtag, index) => (
@@ -58,18 +86,28 @@ export function Post({ author, content, publishedAt }) {
                 })}
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleSubmitForm} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
-                <textarea placeholder="Deixe um comentário" />
+                <textarea
+                    placeholder="Deixe um comentário"
+                    onChange={handleNewComment}
+                    value={newComment}
+                    required
+                />
                 <footer>
                     <button type="submit">Publicar</button>
                 </footer>
             </form>
             <div className="commentList">
-                <Comment />
-                <Comment />
-                <Comment />
+                {comments.map((comment, index)=> {
+                    return (
+                        <Comment
+                            key={index}
+                            content={comment}
+                        />
+                    )
+                })}
             </div>
         </article>
     )
